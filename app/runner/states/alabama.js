c45aec76fab8b6cc0f39a1d5904a3808b35e5ffc
Nyefan <nyefancoding@gmail.com>
2016-05-18 16:39:52 -0500

    Migrate alabama.js

--- app/runner/states/alabama.js
+++ app/runner/states/alabama.js
@@ -1,61 +1,84 @@
+"use strict";
+
+const _ = require('lodash');
 const Q = require('q');
-const URL = "https://myinfo.alabamavotes.gov/VoterView/RegistrantSearch.do";
-const NAME = 'Alabama';
-const INFO = {
-    'firstName':'#nameFirst',
-    'lastName':'#nameLast',
-    'dobMonth':'#dobMonth',
-    'dobDay':'#dobDay',
-    'dobYear':'#dobYear',
-    'county':'#county',
-    'submit':'input[type="submit"]',
-    'status': '#registrant > span:nth-of-type(4)',
-    'expectedStatus':'ACTIVE'
-};
+const help = require('../lib/utils.js');
 
-var user = {
-    'county':'Montgomery',
-    'firstName':'Terri',
-    'lastName':'Sewell',
-    'birthdate':'01/01/1965',
-    'expectedParty':'Democratic'
-};
-
-var verifyRegistration = function(client, user) {
-	try {
-		if (user && typeof user['birthdate'] === "string") {
-			var splitBirth = user['birthdate'].split('/');
-			user['dobYear'] = splitBirth[2];
-			user['dobDay'] = splitBirth[1];
-			user['dobMonth'] = splitBirth[0];
+let state = {
+	URL: "https://myinfo.alabamavotes.gov/VoterView/RegistrantSearch.do",
+	default: {
+		'county':'Montgomery',
+		'firstName':'Terri',
+		'lastName':'Sewell',
+		'birthdate':'01/01/1965'
+	},
+	selectorMap: {
+		'county':'#county',
+		'firstName':'#nameFirst',
+		'lastName':'#nameLast',
+		'dobMonth':'#dobMonth',
+		'dobDay':'#dobDay',
+		'dobYear':'#dobYear',
+		'submit':'input.search-button',
+		'status': '#registrant > span:nth-of-type(4)',
+		'expectedStatus':'ACTIVE'
+	},
+	_init: function() {
+		_.bindAll(this, 'verifyRegistration', 'manipulateData', 'script');
+	},
+	verifyRegistration: function(driver, user) {
+		if (!driver || !user) {
+			console.error(driver, user);
+			throw new Error('required arguments missing, driver, user');
 		}
-	}
-	catch(error) {
-		console.error(error);
-	}
 
-	return Q.promise(function(resolve, reject) {
-		client
-			.url(URL)
-			.setValue(INFO['firstName'], user['firstName'])
-			.setValue(INFO['lastName'], user['lastName'])
-			.selectByValue(INFO['dobMonth'], user['dobMonth'])
-                        .selectByValue(INFO['dobYear'], user['dobYear'])
-                        .selectByValue(INFO['dobDay'], user['dobDay'])
-                        // I believe this works, but I don't have a valid test case with a
-                        // county attached.
-                        //.selectByVisibleText(INFO['county'], user['county'])
-			.click(INFO['submit'])
-			.waitForExist(INFO['status'], 5000)
-			.getText(INFO['status'])
-			.then(function(value) {
-				resolve(INFO['expectedStatus'] === value.trim());
+		this.driver = driver;
+		this.user = this.manipulateData(user);
+
+		let script = this.script;
+		return Q.promise(script);
+	},
+
+	manipulateData: function(u) {
+		// If any UserInfo needs massaging...
+		let splitDate = u.birthdate.split('/');
+
+		u.dobMonth = splitDate[0];
+		u.dobDay = splitDate[1];
+		u.dobYear = splitDate[2];
+
+		return u;
+	},
+
+	script: function(resolve, reject) {
+		let d = this.driver;
+		let i = this.selectorMap;
+		let u = this.user;
+		let URL = this.URL;
+
+		help.goTo(d, URL);
+		Object.keys(i)
+			.forEach(function(key) {
+				if (i[key] && u[key]) {
+					help.setValue(d, i[key], u[key]);
+				}
 			});
-	});
+
+		help.findElement(d, i['submit'])
+			.then(function(element) {
+				element.click();
+				return help.findElement(d, i['status']); // Promise
+			})
+			.then(function(element) {
+				return element.getText(); // Promise
+			})
+			.then(function(text) {
+				let result = (text.trim() === i['expectedStatus']);
+				(result) ? resolve('Active') : reject('Inactive');
+			});
+	}
 };
 
-module.exports = {
-	verifyRegistration:verifyRegistration,
-        user:user,
-        name:NAME
-};
+state._init();
+
+module.exports = state;
